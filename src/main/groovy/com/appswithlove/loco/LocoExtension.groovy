7ef23d66fbd2cf589d7d2a5a5bd2b5e531f811bb
Yannick Pulver <yannick.pulver@gmail.com>
2019-01-03 11:53:46 +0100

    initial version

---
+++ src/main/groovy/com/appswithlove/loco/LocoExtension.groovy
@@ -0,0 +1,10 @@
+package com.appswithlove.loco
+
+class LocoExtension {
+    public static final NAME = 'Loco'
+    def locoBaseUrl = "https://localise.biz/api/export/locale"
+    def apiKey
+    def lang = ['en']
+    def defLang = 'en'
+    def resDir
+}
---
+++ src/main/groovy/com/appswithlove/loco/LocoPlugin.groovy
@@ -0,0 +1,12 @@
+package com.appswithlove.loco
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class LocoPlugin implements Plugin<Project> {
+    @Override
+    void apply(Project target) {
+        target.extensions.create(LocoExtension.NAME, LocoExtension)
+        target.tasks.create(LocoTask.NAME, LocoTask)
+    }
+}
