60a531b3f71a945f7b36242c710846c50cedc980
Yannick Pulver <yannick.pulver@gmail.com>
2019-03-05 09:24:40 +0100

    Release 0.0.3, fixing #3

--- src/main/groovy/com/appswithlove/loco/LocoTask.groovy
+++ src/main/groovy/com/appswithlove/loco/LocoTask.groovy
@@ -23,10 +23,13 @@ class LocoTask extends DefaultTask {
             connection.with {
                 doOutput = true
                 requestMethod = 'GET'
-                new File("${project.Loco.resDir}/values-$lang/strings.xml").text = content.text.replaceAll(/\$[^$]*\$/, "%s")
+               
+                def file = new File("${project.Loco.resDir}/values-$lang/strings.xml")
+                file.write(content.text.replaceAll(/\$[^$]*\$/, "%s"), 'utf-8')
 
                 if (lang == project.Loco.defLang) {
-                    new File("${project.Loco.resDir}/values/strings.xml").text = new File("${project.Loco.resDir}/values-$lang/strings.xml").text
+                    def internalFile = new File("${project.Loco.resDir}/values/strings.xml")
+                    internalFile.write(file.text, 'utf-8')
                 }
             }
         }
