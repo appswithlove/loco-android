1ca56894a4e55ce61531ec13221f08f82d899da4
anthony.fresneau.kiplin <anthony.fresneau@kiplin.com>
2021-10-20 11:26:58 +0200

    Add possibility to import 2 or more loco file For a single file, it remains compatible with the actual task updateLoco and the actual configuration syntax For multiple files, the task updateLocoMultiple is added and a sample configuration block is added in README.md

--- src/main/groovy/com/appswithlove/loco/LocoExtension.groovy
+++ src/main/groovy/com/appswithlove/loco/LocoExtension.groovy
@@ -1,16 +1,9 @@
 package com.appswithlove.loco
 
 class LocoExtension {
-    public static final NAME = 'Loco'
-    def locoBaseUrl = "https://localise.biz/api/export/locale"
-    def apiKey
-    def lang = ['en']
-    def defLang = 'en'
-    def resDir
-    def fileName = "strings"
-    def placeholderPattern = null
-    def hideComments = false
-    def tags
-    def fallbackLang = null
-    def orderByAssetId = false
+    public static final String NAME = 'Loco'
+    public static final String MULTIPLE_NAME = 'LocoMultiple'
+
+    // Array of config
+    public LocoConfig[] configs
 }
