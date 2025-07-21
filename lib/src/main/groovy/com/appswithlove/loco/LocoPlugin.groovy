f527dd6005edb95df3f6d140db7e114a5336f7c8
Vladislav Frolov <f.vladislav.k@gmail.com>
2025-07-21 17:14:59 +0200

    Add Gradle Configuration Cache support

--- lib/src/main/groovy/com/appswithlove/loco/LocoPlugin.groovy
+++ lib/src/main/groovy/com/appswithlove/loco/LocoPlugin.groovy
@@ -7,6 +7,9 @@ class LocoPlugin implements Plugin<Project> {
     @Override
     void apply(Project target) {
         target.extensions.create(LocoExtension.NAME, LocoExtension, target)
-        target.tasks.create(LocoTask.NAME, LocoTask)
+        target.tasks.register(LocoTask.NAME, LocoTask) { task ->
+            def extension = project.extensions.getByType(LocoExtension)
+            task.configList = extension.configList
+        }
     }
 }
--- lib/src/main/groovy/com/appswithlove/loco/LocoTask.groovy
+++ lib/src/main/groovy/com/appswithlove/loco/LocoTask.groovy
@@ -2,7 +2,7 @@ package com.appswithlove.loco
 
 import org.gradle.api.DefaultTask
 import org.gradle.api.GradleException
-import org.gradle.api.tasks.Internal
+import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.TaskAction
 
 /**
@@ -12,23 +12,17 @@ class LocoTask extends DefaultTask {
     // Name of the task
     public static String NAME = 'updateLoco'
 
-    @Internal
-    LocoExtension extension
-
-    LocoTask() {
-        project.afterEvaluate {
-            extension = project.extensions."${LocoExtension.NAME}"
-        }
-    }
+    @Input
+    List<String> configList = []
 
     @TaskAction
     void doLast() {
-        if (extension.configList.size() == 0) {
+        if (configList.isEmpty()) {
             throw new GradleException("Could not find any Loco Configs.")
         }
 
         // Iterate on each configuration and generate xml file based on it
-        extension.configList.each {
+        configList.each {
             TaskUtils.generate(it)
         }
 
