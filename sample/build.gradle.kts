451f419d13a09f847d865630a92dfacfeba18666
Vladislav Frolov <f.vladislav.k@gmail.com>
2025-10-13 18:42:14 +0200

    update java in sample project

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -34,13 +34,13 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 
     tasks.withType<KotlinJvmCompile>().configureEach {
         compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_11)
+            jvmTarget.set(JvmTarget.JVM_1_8)
             freeCompilerArgs.add("-opt-in=kotlin.RequiresOptIn")
         }
     }
